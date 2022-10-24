78de0d86809a62d2875003497dd2e5b0137efbc8
Muhammad Osama Javaid <18pwcse1638@uetpeshawar.edu.pk>
2022-10-24 23:39:27 +0500

    ready to deploy

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -12,7 +12,7 @@ function ExpenseItem(props){
 
   return (
     <div className='my-6 flex justify-center items-center'>
-        <Card className='flex bg-blue-900 px-4 space-x-3 md:space-x-6 lg:space-x-10 sm:bg-red-500 w-[80%] sm:w-[50%] md:w-[60%] lg:w-[70%] h-28 md:bg-slate-700 lg:bg-red-500 xl:bg-fuchsia-700 xl:space-x-16  items-center justify-between sm:px-1 md:px-3 lg:px-5 xl:px-5 shadow-2xl'>
+        <Card className='flex px-4 space-x-3 md:space-x-6 lg:space-x-10 w-[80%] sm:w-[50%] md:w-[60%] lg:w-[70%] h-28 bg-slate-700 xl:space-x-16  items-center justify-between sm:px-1 md:px-3 lg:px-5 xl:px-5 shadow-2xl'>
           <ExpandDate date={props.date}/>
         <div className='flex-1'>
             <h1 className='text-white sm:text-sm md:text-xl lg:text-2xl font-bold'>{props.title}</h1>
